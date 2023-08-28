b91a786ac6f39c7281866e9786d7c5e2641d9862
kvineetkumar <kvineetkumar32@gmail.com>
2023-08-28 17:57:58 +0530

    resolved issue with MyKartServiceDiscoveryApplication

---
+++ MyKartServiceDiscovery/src/test/java/com/mykart/services/MyKartServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykart.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKartServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
